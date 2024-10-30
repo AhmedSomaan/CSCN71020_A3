da227c66015e2c2c8ef02880c54025326bc9260b
Ahmed Somaan <ahmedsomaan7@gmail.com>
2024-10-30 19:30:57 -0400

    created unit tests to check invalid inputs for both players including empty inputs

--- GameWinnerTest/GameWinnerTest.cpp
+++ GameWinnerTest/GameWinnerTest.cpp
@@ -12,7 +12,7 @@ namespace GameWinnerTest
 	public:
 		TEST_METHOD(TestInvalidPlayer1)
 		{
-			const char* player1Input = "rock";// lowercase R in rock
+			const char* player1Input = "rock";// lowercase R in Rock invalid
 			const char* player2Input = "Paper";
 
 			const char* output = GetWinner(player1Input, player2Input);
@@ -20,5 +20,62 @@ namespace GameWinnerTest
 
 			Assert::AreEqual(expectedOutput, output);
 		}
+
+		TEST_METHOD(TestInvalidPlayer2)
+		{
+			const char* player1Input = "Rock";
+			const char* player2Input = "paper";// lowercase P in Paper invalid
+
+			const char* output = GetWinner(player1Input, player2Input);
+			const char* expectedOutput = "Invalid";
+
+			Assert::AreEqual(expectedOutput, output);
+		}
+
+		TEST_METHOD(TestBothInvalid)
+		{
+			const char* player1Input = "abc";
+			const char* player2Input = "def";
+
+			const char* output = GetWinner(player1Input, player2Input);
+			const char* expectedOutput = "Invalid";
+
+			Assert::AreEqual(expectedOutput, output);
+		}
+
+		TEST_METHOD(TestInvalidPlayer1Empty)
+		{
+			const char* player1Input = "";
+			const char* player2Input = "Paper";
+
+			const char* output = GetWinner(player1Input, player2Input);
+			const char* expectedOutput = "Invalid";
+
+			Assert::AreEqual(expectedOutput, output);
+		}
+
+		TEST_METHOD(TestInvalidPlayer2Empty)
+		{
+			const char* player1Input = "Rock";
+			const char* player2Input = "";
+
+			const char* output = GetWinner(player1Input, player2Input);
+			const char* expectedOutput = "Invalid";
+
+			Assert::AreEqual(expectedOutput, output);
+		}
+
+		TEST_METHOD(TestInvalidBothEmpty)
+		{
+			const char* player1Input = "";
+			const char* player2Input = "";
+
+			const char* output = GetWinner(player1Input, player2Input);
+			const char* expectedOutput = "Invalid";
+
+			Assert::AreEqual(expectedOutput, output);
+		}
+
+
 	};
 }
