e5ebed5be17d2fba415c519874c6a65ccc0a3adf
Ahmed Somaan <ahmedsomaan7@gmail.com>
2024-10-30 21:30:22 -0400

    Created main function logic for draw condition and fixed logic for invalid input

--- CSCN71020_A3/main.c
+++ CSCN71020_A3/main.c
@@ -7,8 +7,12 @@ int main() {
 }
 
 const char* GetWinner(const char* player1, const char* player2) {
-	if ((strcmp(player1, "Rock") != 0) || (strcmp(player1, "Paper") != 0) || (strcmp(player1, "Scissors") != 0)
-		|| (strcmp(player2, "Rock") != 0) || (strcmp(player2, "Paper") != 0) || (strcmp(player2, "Scissors") != 0)) {
+	if ((strcmp(player1, "Rock") != 0 && strcmp(player1, "Paper") != 0 && strcmp(player1, "Scissors") != 0)
+		|| (strcmp(player2, "Rock") != 0 && strcmp(player2, "Paper") != 0 && strcmp(player2, "Scissors") != 0)) {
 		return "Invalid";
+	}
+
+	if (strcmp(player1, player2) == 0) {
+		return "Draw";
 	}
 }
