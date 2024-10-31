1e45299d5a69919a343e3416aa8a1657df6aae20
Ahmed Somaan <ahmedsomaan7@gmail.com>
2024-10-30 21:13:24 -0400

    created main function logic for determining if input is invalid

--- CSCN71020_A3/main.c
+++ CSCN71020_A3/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "main.h"
 
 int main() {
@@ -6,5 +7,8 @@ int main() {
 }
 
 const char* GetWinner(const char* player1, const char* player2) {
-
+	if ((strcmp(player1, "Rock") != 0) || (strcmp(player1, "Paper") != 0) || (strcmp(player1, "Scissors") != 0)
+		|| (strcmp(player2, "Rock") != 0) || (strcmp(player2, "Paper") != 0) || (strcmp(player2, "Scissors") != 0)) {
+		return "Invalid";
+	}
 }
